1f673c38bc6738aa2e7f47cfbbf227284cae9795
Paul Dolden <paul.dolden@thefa.com>
2023-12-20 17:07:48 +0000

    wip

--- 2023/20.go
+++ 2023/20.go
@@ -1,6 +1,7 @@
 package _2023
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/pauldolden/advent-go/config"
@@ -8,64 +9,115 @@ import (
 )
 
 type broadcast struct {
-	sender     string
-	recipients []string
-	// 0 = low, 1 = high
-	pulse int
+	sender    string
+	recipient string
 }
 
 type module struct {
-	name   string
-	pulse  int
-	inputs []module
-}
-
-type queue []broadcast
-
-func (q *queue) enqueue(b broadcast) {
-	*q = append(*q, b)
-}
-
-func (q *queue) dequeue() broadcast {
-	b := (*q)[0]
-	*q = (*q)[1:]
-	return b
+	inputs []string
+	mode   string
 }
 
 func TwentyOne(o config.Options) int {
 	scanner, file := utils.OpenFile(2023, 20, o)
 	defer file.Close()
-	// high level overview we can snapshot this, so we can see when a cycle is in the initial state
-	// state := make(map[string]int)
-	depMap := make(map[string]module)
-
-	// we just take the first line to get the broadcaster
-	scanner.Scan()
-	// broadcaster
-	b := scanner.Text()
-	broadcaster := parseBroadcast(b)
-	q := queue{}
-	q.enqueue(broadcaster)
+	pulseMap := make(map[string]int)
+	inputMap := make(map[string]module)
+	var broadcasts [][]broadcast
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		b := parseBroadcast(line)
+		parseInputs(line, inputMap)
+		parseMode(line, inputMap)
+		parsePulses(line, pulseMap)
+
+		b := parseBroadcasts(line, pulseMap)
+
+		broadcasts = append(broadcasts, b)
+	}
+
+	for _, b := range broadcasts {
+		for _, r := range b {
+			switch inputMap[r.recipient].mode {
+			case "initial":
+				fmt.Println(r.recipient)
+			case "%":
+				fmt.Println(r.recipient)
+			case "&":
+				fmt.Println(r.recipient)
+			}
+		}
 	}
 
 	return 0
 }
 
-func parseBroadcast(s string, pulseMap) broadcast {
+func parseMode(s string, inputMap map[string]module) {
 	ss := strings.Split(s, " -> ")
 	sender := ss[0]
-	recipients := strings.Split(ss[1], ", ")
+	var mode string
 
+	if strings.Contains(sender, "%") || strings.Contains(sender, "&") {
+		mode = sender[0:1]
+		sender = sender[1:]
+	} else {
+		mode = "initial"
+	}
 
+	module := inputMap[sender]
+	module.mode = mode
+	inputMap[sender] = module
+}
 
-	return broadcast{
-		sender:     sender,
-		recipients: recipients,
-		pulse:      0,
+func parseBroadcasts(s string, pulseMap map[string]int) []broadcast {
+	var broadcasts []broadcast
+	ss := strings.Split(s, " -> ")
+	sender := ss[0]
+
+	if strings.Contains(sender, "%") || strings.Contains(sender, "&") {
+		sender = sender[1:]
+	}
+
+	for _, r := range strings.Split(ss[1], ", ") {
+		broadcasts = append(broadcasts, broadcast{
+			sender:    sender,
+			recipient: r,
+		})
+	}
+
+	return broadcasts
+}
+
+func parsePulses(s string, pulseMap map[string]int) {
+	ss := strings.Split(s, " -> ")
+	sender := ss[0]
+
+	if strings.Contains(sender, "%") || strings.Contains(sender, "&") {
+		sender = sender[1:]
+	}
+
+	pulseMap[sender] = 0
+}
+
+func parseInputs(s string, inputMap map[string]module) {
+	ss := strings.Split(s, " -> ")
+	sender := ss[0]
+
+	if strings.Contains(sender, "%") || strings.Contains(sender, "&") {
+		sender = sender[1:]
+	}
+	for _, r := range strings.Split(ss[1], ", ") {
+		if _, ok := inputMap[r]; !ok {
+			inputMap[r] = module{
+				inputs: []string{sender},
+				mode:   "initial",
+			}
+		} else {
+			inputMap[r] = module{
+				inputs: append(inputMap[r].inputs, sender),
+				mode:   "initial",
+			}
+		}
 	}
 }
 
